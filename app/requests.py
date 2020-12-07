85746efd8e8c42056aa884fbe5937151211cf48a
Ornella-KK <ornellakankazi@gmail.com>
2020-12-07 11:48:40 +0200

    added quote url and a way to retrueve them

--- app/requests.py
+++ app/requests.py
@@ -1,46 +1,18 @@
-from app import app
+import urllib.request, json
+from .models import Quote
+from config import Config
+import requests
 
-# Getting the movie base url
-base_url = None
-
-def configure_request(app):
-    global base_url
-    base_url = app.config['QUOTES_API_BASE_URL']
-
-def get_quotes():
+quote_url = Config.QUOTE_API_BASE_URL
+    
+def getQuotes():
     '''
-    Function that gets the json response to our url request
+    function that gets the json response  quotes to our url request
     '''
-    get_quotes_url = base_url
-
-    with urllib.request.urlopen(get_quotes_url) as url:
-        get_movies_data = url.read()
-        get_movies_response = json.loads(get_movies_data)
-
-        quote_results = None
-        quote_results_list = get_quotes_response['results']
-        quote_results = process_results(quote_results_list)
-
-
-    return quote_results
-
-def process_results(quote_list):
-    '''
-    Function  that processes the quote result and transform them to a list of Objects
-
-    Args:
-        quote_list: A list of dictionaries that contain quote details
-
-    Returns :
-        quote_results: A list of quote objects
-    '''
-    quote_results = []
-    for quote_item in quote_list:
-        author = quote_item.get('author')
-        quote = quote_item.get('quote')
-
-        if author:
-            quote_object = Quote(author,quote)
-            quote_results.append(quote_object)
-
-    return quote_results
+    quote=requests.get(quote_url)
+    new_quote = quote.json()
+    author = new_quote.get('author')
+    quote = new_quote.get('quote')
+    quote_result = Quote(author, quote)
+        
+    return quote_result     
