095bdca2db49b681a85f7bffa41f65d2f9102ac0
Ornella-KK <ornellakankazi@gmail.com>
2020-12-07 11:40:38 +0200

    added configs

--- config.py
+++ config.py
@@ -1,20 +1,41 @@
 import os
 
 class Config:
-
-    QUOTES_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    '''
+    General configuration parent class
+    '''
+    QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ornella:kko@localhost/blog'
+    SECRET_KEY='secret'
+    # UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "ornellakankazi@gmail.com"
+    MAIL_PASSWORD = "1234"
+    
 
 class ProdConfig(Config):
-    QUOTES_API_BASE_URL ='http://quotes.stormconsultancy.co.uk/random.json'
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    '''
+    Production  configuration child class
 
-
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+        
 class DevConfig(Config):
-    DEBUG = True
+    '''
+    Development  configuration child class
 
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True    
+
+config_options ={
+    'development':DevConfig,
+    'production':ProdConfig,
 }
